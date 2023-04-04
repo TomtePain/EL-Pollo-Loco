3d97beb73e9fdd026322e0868d9aa31b5f21789d
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-04-04 17:48:41 +0200

    added move function

--- js/game.js
+++ js/game.js
@@ -4,12 +4,44 @@ let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
     
 
     console.log('My Character is', world.character)
 }
 
-window.addEventListener("keypress", (e) => {
-    console.log(e);
+window.addEventListener("keydown", (e) => {
+    if(e.keyCode == 39) {
+        keyboard.right = true;
+    }
+    if(e.keyCode == 37) {
+        keyboard.left = true;
+    }
+    if(e.keyCode == 38) {
+        keyboard.up = true;
+    }
+    if(e.keyCode == 40) {
+        keyboard.down = true;
+    }
+    if(e.keyCode == 32) {
+        keyboard.space = true;
+    }
+})
+
+window.addEventListener("keyup", (e) => {
+    if(e.keyCode == 39) {
+        keyboard.right = false;
+    }
+    if(e.keyCode == 37) {
+        keyboard.left = false;
+    }
+    if(e.keyCode == 38) {
+        keyboard.up = false;
+    }
+    if(e.keyCode == 40) {
+        keyboard.down = false;
+    }
+    if(e.keyCode == 32) {
+        keyboard.space = false;
+    }
 })
--- models/character.class.js
+++ models/character.class.js
@@ -12,7 +12,7 @@ class Character extends MovableObject {
         'img_pollo_locco/2_character_pepe/2_walk/W-25.png',
         'img_pollo_locco/2_character_pepe/2_walk/W-26.png'
     ];
-
+    world;
 
     constructor() {
         super().loadImg('img_pollo_locco/2_character_pepe/2_walk/W-21.png');
@@ -23,12 +23,31 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
+            this.speed = 2,5;
+            if(this.world.keyboard.right) {
+                this.x += this.speed;
+            }
+
+            if(this.world.keyboard.left) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+
+        setInterval(() => {
+
+            if(this.world.keyboard.right || this.world.keyboard.left) {
+            
             let i = this.currentImage % this.Images_Walking.length;
             let path = this.Images_Walking[i];
             this.img = this.imageCache[path];
             this.currentImage++;
+            
+            }
         }, 100);
+    
     }
 
     jump() {
