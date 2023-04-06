76c05a32db9b55471da8639fe2e7286c4a8d7cf1
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-04-06 17:26:47 +0200

    added level system

--- models/world.class.js
+++ models/world.class.js
@@ -2,29 +2,9 @@ class World {
 
 
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Clouds('img_pollo_locco/5_background/layers/4_clouds/1.png')
-    ]
-    backgroundObjects = [
-        new BackgroundObject('img_pollo_locco/5_background/layers/air.png', 0),
-        new BackgroundObject('img_pollo_locco/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img_pollo_locco/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img_pollo_locco/5_background/layers/1_first_layer/1.png', 0),
-
-        new BackgroundObject('img_pollo_locco/5_background/layers/air.png', 719),
-        new BackgroundObject('img_pollo_locco/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img_pollo_locco/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img_pollo_locco/5_background/layers/1_first_layer/2.png', 719),
-
-        new BackgroundObject('img_pollo_locco/5_background/layers/air.png', 1438),
-        new BackgroundObject('img_pollo_locco/5_background/layers/3_third_layer/1.png', 1438),
-    
-    ]
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
