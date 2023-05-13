edeb89aa8f82e5287fcaf2691616d2f23a91b630
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-05-13 11:00:28 +0200

    added bottle statusbar images

--- models/statusbar-bottle.class.js
+++ models/statusbar-bottle.class.js
@@ -4,12 +4,12 @@ class Statusbar_Bottle extends DrawableObject {
     height = 40;
 
     Images_Bottle = [
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/0.png',
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/20.png',
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/40.png',
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/60.png',
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/80.png',
+        'img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/100.png'
     ]
 
     percentage = 0;
@@ -27,6 +27,9 @@ class Statusbar_Bottle extends DrawableObject {
         this.percentage = percentage;
         let imagePath = this.Images_Bottle[this.resolveImageIndex()];
         this.img = this.imageCache[imagePath];
+        if(this.percentage > 5) {
+            this.loadImg(`img_pollo_locco/7_statusbars/1_statusbar/3_statusbar_bottle/orange/bottl/${this.percentage}.png`)
+        }
     }
 
     resolveImageIndex() {
